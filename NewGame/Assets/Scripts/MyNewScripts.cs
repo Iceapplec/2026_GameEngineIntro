d0185ffe5eb8759edf7572d9b600df964fb88fee
Iceapplec <dhpinkanny0222@gmail.com>
2026-03-11 16:21:19 +0900

    1234

---
+++ NewGame/Assets/Scripts/MyNewScripts.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MyNewScripts : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //void Start()
+    //{
+    //    Debug.Log("안녕하세여");
+    //    Debug.LogWarning("위험");
+    //    Debug.LogError("에러!");
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
